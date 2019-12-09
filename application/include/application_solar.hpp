d62abead709ab50896fd5b7247477e592a2dcacc
Vanessa Retz <vanessa.retz@aol.com>
2019-12-09 23:43:56 +0100

    add shaders (frag/vert), initialize stars in application solar, running but needs fixing

--- application/include/application_solar.hpp
+++ application/include/application_solar.hpp
@@ -8,6 +8,8 @@
 #include "scenegraph.hpp"
 #include "geometry_node.hpp"
 
+#include <vector>
+
 // gpu representation of model
 class ApplicationSolar : public Application {
  public:
@@ -29,6 +31,7 @@ class ApplicationSolar : public Application {
  protected:
   void initializeShaderPrograms();
   void initializeGeometry();
+  void initializeStarGeometry();
   // update uniform values
   void uploadUniforms();
 
@@ -39,7 +42,7 @@ class ApplicationSolar : public Application {
   void addMoon(std::string const& planet_name, std::string const& moon_name);
 
   void renderPlanets() const;
-
+  void renderStars() const;
   // upload projection matrix
   void uploadProjection();
   // upload view matrix
@@ -47,6 +50,7 @@ class ApplicationSolar : public Application {
 
   // cpu representation of model
   model_object planet_object;
+  model_object star_object;
   
   // camera transform matrix
   glm::fmat4 m_view_transform;
@@ -54,6 +58,8 @@ class ApplicationSolar : public Application {
   glm::fmat4 m_view_projection;
 
   Scenegraph scenegraph_;
+
+  std::vector<float> starsVec_;
 };
 
 #endif
